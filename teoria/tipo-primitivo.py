146e13d2e0ab1ce035fd7cb3a79c5ce9b89ea01e
FernandaMakiHirose <femahi2020@gmail.com>
2021-04-23 10:33:22 -0300

    Adicionando tipo primitivo

---
+++ teoria/tipo-primitivo.py
@@ -0,0 +1,27 @@
+"""
++ soma
+- subtração
+* multiplicação
+/ divisão
+** potência
+// é o resultado de uma divisão sem a vírgula
+% é o resto, se o resto for igual a 1, o número é ímpar, se for igual a 0 o número é par
+== igual
+
+
+a ordem de precedência (que precisa fazer o cálculo primeiro) é:
+1- () parênteses
+2- ** potência
+3- * multiplicação, / divisão, // resultado da divisão inteira, % resto da divisão inteira (o que aparecer primeiro é o que vai calcular primeiro)
++ adição e - subtração
+
+
+(com o type conseguimos descobrir qual é o tipo primitivo)
+exemplo:
+num = 5
+type(num)
+
+
+para números muito grandes, para facilitar nossa leitura podemos separar por underline: (o exemplo abaixo é um milhão)
+1_000_000
+"""
