0e8c1f774c0e56abeaa53c91c0173509364dfe44
FernandaMakiHirose <femahi2020@gmail.com>
2021-04-23 12:40:42 -0300

    Estruturas lógicas

---
+++ teoria/estruturas-logicas.py
@@ -0,0 +1,41 @@
+"""
+✔️ Estruturas lógicas: and, or, not, is
+
+Operadores unários: not
+Operadores binários: and, or, is
+
+Para o and os dois valores precisam ser True, para o or um ou outro valor precisa ser True
+O not é o booleano invertido
+
+👍 Exemplo 1:
+
+ativo = True
+logado = True
+
+if ativo and logado: 
+    print('Bem-vindo usuário!')
+else: 
+    print('Você precisa ativar a sua conta, chegue se e-mail')
+
+
+
+👍 Exemplo 2:
+
+ativo = True
+if ativo not True:
+    print('Você precisa ativar a sua conta, cheque seu e-mail')
+else:
+    print('Bem-vindo usuário')
+
+
+👍 Exemplo 3: 
+
+ativo = True
+logado = False
+
+if ativo:
+    print('Bem-vindo usuário')
+else:
+    print('Você precisa ativar a sua conta, cheque seu e-mail')
+print(ativo is True)
+"""
