e21262970f13b633ae098cf23da3c2930690155c
FernandaMakiHirose <femahi2020@gmail.com>
2021-05-01 14:43:23 -0300

    Jogo da cobrinha

--- jogo-da-cobrinha/cobrinha.py
+++ jogo-da-cobrinha/cobrinha.py
@@ -1,10 +1,13 @@
+# jogo snake
 import pygame
 import random
-azul = (50, 100, 213)
+pygame.init()
+azul = (50, 100, 213)  # código rgb
 laranja = (205, 102, 0)
 verde = (0, 255, 0)
+amarelo = (255, 255, 102)
 dimensoes = (600, 600)
-### VALORES INICIAIS ###
+# VALORES INICIAIS
 x = 300
 y = 300
 d = 20
@@ -13,6 +16,7 @@ dx = 0
 dy = 0
 x_comida = round(random.randrange(0, 600 - d) / 20) * 20
 y_comida = round(random.randrange(0, 600 - d) / 20) * 20
+fonte = pygame.font.SysFont("hack", 35)
 tela = pygame.display.set_mode((dimensoes))
 pygame.display.set_caption('Snake da Kenzie')
 tela.fill(azul)
@@ -47,13 +51,35 @@ def verifica_comida(dx, dy, x_comida, y_comida, lista_cobra):
     y_novo = head[1] + dy
     if head[0] == x_comida and head[1] == y_comida:
         lista_cobra.append([x_novo, y_novo])
+        x_comida = round(random.randrange(0, 600 - d) / 20) * 20
+        y_comida = round(random.randrange(0, 600 - d) / 20) * 20
     pygame.draw.rect(tela, verde, [x_comida, y_comida, d, d])
     return x_comida, y_comida, lista_cobra
+def verifica_parede(lista_cobra):
+    head = lista_cobra[-1]
+    x = head[0]
+    y = head[1]
+    if x not in range(600) or y not in range(600):
+        raise Exception
+def verifica_mordeu_cobra(lista_cobra):
+    head = lista_cobra[-1]
+    corpo = lista_cobra.copy()
+    del corpo[-1]
+    for x, y in corpo:
+        if x == head[0] and y == head[1]:
+            raise Exception
+def atualizar_pontos(lista_cobra):
+    pts = str(len(lista_cobra) - 1)
+    escore = fonte.render("Pontuação: " + pts, True, amarelo)
+    tela.blit(escore, [0, 0])
 while True:
     pygame.display.update()
     desenha_cobra(lista_cobra)
     dx, dy, lista_cobra = mover_cobra(dx, dy, lista_cobra)
     x_comida, y_comida, lista_cobra = verifica_comida(
         dx, dy, x_comida, y_comida, lista_cobra)
-    print(lista_cobra)
-    clock.tick(20)
+    verifica_parede(lista_cobra)
+    verifica_mordeu_cobra(lista_cobra)
+    atualizar_pontos(lista_cobra)
+    clock.tick(5)
+    
