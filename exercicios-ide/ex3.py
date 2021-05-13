2919867a465b9064442a5317aa6ced31492d1aff
FernandaMakiHirose <femahi2020@gmail.com>
2021-05-13 17:06:26 -0300

    Novos commits

--- exercicios-ide/ex3.py
+++ exercicios-ide/ex3.py
@@ -2,4 +2,4 @@
 
 n1 = str(input('Digite um número: '))
 n2 = str(input('Digite outro número: '))
-print(f'A soma entre {n1} e {n2} é {n1 + n2}')
+print(f'A soma concatenada entre {n1} e {n2} é {n1 + n2}')
